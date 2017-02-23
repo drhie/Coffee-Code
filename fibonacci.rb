ce409511aeb8f9f751b79e2f70608f03db5b3f62
drhie <rhie.david@gmail.com>
2017-02-23 10:28:22 -0500

    From Coffee & Code 12

--- fibonacci.rb
+++ fibonacci.rb
@@ -1,7 +1,7 @@
 def fibonacci(n)
   fib_array = [0, 1]
   n.times { fib_array << fib_array[-1] + fib_array[-2] }
-  fib_array[-1] + fib_array[-2]
+  fib_array[n-1]
 end
 
-p fibonacci(5) #=> 13
+p fibonacci(5) #=> 3
