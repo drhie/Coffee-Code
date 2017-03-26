511e4eecd54a263fb25d9ccd699878141e456bbb
drhie <rhie.david@gmail.com>
2017-03-25 20:38:21 -0400

    Rework minimum solution to not require array

--- javascript/minimum.js
+++ javascript/minimum.js
@@ -1,9 +1,9 @@
-function min(array) {
-  var minimum = array[0];
-  array.forEach(function(num) {
-    minimum < num ? minimum = minimum : minimum = num;
-  });
+function min() {
+  var minimum = arguments[0];
+  for (i = 0; i < arguments.length; i++) {
+    minimum < arguments[i] ? minimum = minimum : minimum = arguments[i];
+  }
   return minimum;
 }
 
-console.log(min([24, 90, 28, 34, 2, 64, 18, 3]))
+console.log(min(24, 90, 28, 34, 2, 64, 18, 3))
