d7be894798dad8733cf26d77bc11163ef751fdd9
drhie <rhie.david@gmail.com>
2017-03-30 23:50:08 -0400

    Add sandbox files for all languages

---
+++ javascript/sandbox.js
@@ -0,0 +1,14 @@
+//This file exists to play with code and the contents can be deleted.
+
+function test(e) {
+  if (e === 5) {
+    console.log("This is five!")
+  } else if (e > 5) {
+    console.log("This is bigger than five!")
+  } else {
+    // debugger;
+    console.log("This is smaller than five.")
+  }
+}
+var me = 4
+test(me)
